f6340b2cdfec2ac055d3041e4097f77d10b7b809
Chris Beddome <chrisbeddome@gmail.com>
2023-10-06 17:49:07 -0400

    more accurate heuristic for approot

--- src/pathHelpers.js
+++ src/pathHelpers.js
@@ -1,6 +1,7 @@
 import path from 'path'
 import url from 'url'
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
-const __approot = path.resolve(__dirname).split('node_modules')[0]
+// const __approot = path.resolve(__dirname).split('node_modules')[0]
+const __approot = process.cwd()
 
 export {__dirname, __approot}
