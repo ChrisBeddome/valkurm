079b206224ec4d23b900ecb7c390419ae98da3dc
Chris Beddome <chrisbeddome@gmail.com>
2023-10-06 16:02:04 -0400

    proper error handling on async functionss

--- bin/generateDataMigration.js
+++ bin/generateDataMigration.js
@@ -3,7 +3,7 @@ import generateMigration from '../src/generateMigration.js'
 const userInput = process.argv[2]
 const migrationDirName = "data"
 try {
-  generateMigration(migrationDirName, userInput)
+  await generateMigration(migrationDirName, userInput)
 } catch(e) {
   console.log(e.message)
   process.exit(1)
