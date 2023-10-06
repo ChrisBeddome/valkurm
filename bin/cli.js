6cdea7a8106af300a16beff166189b3c00dbb91f
Chris Beddome <chrisbeddome@gmail.com>
2023-10-06 17:48:48 -0400

    refacctor + better error handling

--- bin/cli.js
+++ bin/cli.js
@@ -1,25 +1,40 @@
 #!/usr/bin/env node
 
-const command = process.argv[2] 
-const args = process.argv.slice(3)
+import UserError from '../src/userError.js'
 
-const executables = {
+const EXECUTABLES = {
   'generate-schema-migration': '../src/generateSchemaMigration.js'
+}
+
+const command = process.argv[2] 
+const commandArgs = process.argv.slice(3)
+
+const run = async command => {
+  try {
+    const module = await import(EXECUTABLES[command]);
+    module.default(...commandArgs)
+  } catch(e) {
+    if (e instanceof UserError) {
+      console.error(e.message);
+    } else {
+      console.error(e)
+    }
+    proccess.exit(1)
+  }
 }
 
 if (!command) {
   console.error('\nUsage: [command] [options]\n')
   console.error('Commands: ')
-  for (const command of Object.keys(executables)) {
+  for (const command of Object.keys(EXECUTABLES)) {
     console.error(`  ${command}`);
   }
   console.error('')
   process.exit(1)
 }
 
-if (executables.hasOwnProperty(command)) {
-  const module = await import(executables[command]);
-  module.default(args)
+if (EXECUTABLES.hasOwnProperty(command)) {
+  run(command)
 } else {
   console.error(`\nCommand '${command}' not recognized.\n`)
   process.exit(1)
