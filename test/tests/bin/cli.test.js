866699f50abe9c0306c6eef15a93115f6eaed3d5
Chris Beddome <chrisbeddome@gmail.com>
2023-10-10 12:27:03 -0400

    work on specs

--- test/tests/bin/cli.test.js
+++ test/tests/bin/cli.test.js
@@ -1,40 +1,72 @@
 import {exec, spawn} from 'child_process'
 
+const runCommand = commandArgs => {
+  let output = {
+    stdout: '',
+    stderr: ''
+  }
+  const process = spawn("npm", ["run", "valkurm", ...commandArgs ])
+  process.stdout.on("data", data => {
+    output.stdout += data.toString()
+  })
+  process.stderr.on("data", data => {
+    output.stderr += data.toString()
+  })
+  return [process, output]
+} 
+
+const shouldExitWithCode = (expectedCode, process , done) => {
+  process.on("exit", code => {
+    try {
+      expect(code).toBe(expectedCode);
+      done()
+    } catch(error) {
+      done(error)
+    }
+  });
+}
+
+const outputShouldMatch = (output, key, matcher, process, done) => {
+  process.on("exit", _code => {
+    try {
+      expect(output[key]).toMatch(matcher)
+      done()
+    } catch(error) {
+      done(error)
+    }
+  });
+}
+
 describe("entry executable", () => {
+
+  let process, output
+
   describe("when given no command", () => {
-    let process
-    let stdout
-    let stderr 
     beforeEach(() => {
-      stdout = ''
-      stderr = ''
-      process = spawn("npm", ["run", "valkurm"])
-      process.stdout.on("data", data => {
-        stdout += data.toString()
-      })
-      process.stderr.on("data", data => {
-        stderr += data.toString()
-      })
+      [process, output] = runCommand([])
     })
-    it("should exit with code 1", done => {
-      process.on("exit", code => {
-        try {
-          expect(code).toBe(1);
-          done()
-        } catch(error) {
-          done(error)
-        }
-      });
-    });
+
+    it(`should exit with code 1`, done => {
+      shouldExitWithCode(1, process, done)
+    })
+
     it("should output usage information", done => {
-      process.on("exit", _code => {
-        try {
-          expect(stderr).toMatch(/Usage:/)
-          done()
-        } catch(error) {
-          done(error)
-        }
-      });
+      outputShouldMatch(output, 'stderr', /Usage:/, process, done)
     })
   })
+  
+  describe("when given invalid command", () => {
+    beforeEach(() => {
+      [process, output] = runCommand(["sdfsdf"])
+    })
+
+    it(`should exit with code 1`, done => {
+      shouldExitWithCode(1, process, done)
+    })
+
+    it("should output usage information", done => {
+      outputShouldMatch(output, 'stderr', /Command 'sdfsdf' not recognized/, process, done)
+    })
+  })
+
 })
