fda4905ad02eab9372aec1bfd8c9a5a57148cf76
Chris Beddome <chrisbeddome@gmail.com>
2023-10-11 08:00:46 -0400

    setup for fileIO testing

--- test/tests/bin/cli.test.js
+++ test/tests/bin/cli.test.js
@@ -22,28 +22,34 @@ const outputFromCommand = (command, fileDescriptor) => {
   return output
 }
 
-describe("entry executable", () => {
-  describe("when given no command", () => {
+describe('entry executable', () => {
+  describe('when given no command', () => {
     const command = 'npm run valkurm'
     it(`should exit with code 1`, () => {
       const code = exitCodeFromCommand(command)
       expect(code).toBe(1)
     })
-    it("should output usage information", () => {
+    it('should output usage information', () => {
       const stderr = outputFromCommand(command, 'stderr')
       expect(stderr).toMatch(/Usage:/)
     })
   })
   
-  describe("when given invalid command", () => {
+  describe('when given invalid command', () => {
     const command = 'npm run valkurm this-is-an-invalid-command'
     it(`should exit with code 1`, () => {
       const code = exitCodeFromCommand(command)
       expect(code).toBe(1)
     })
-    it("should output usage information", () => {
+    it('should output usage information', () => {
       const stderr = outputFromCommand(command, 'stderr')
       expect(stderr).toMatch(/Command 'this-is-an-invalid-command' not recognized/)
+    })
+  })
+
+  describe('when given valid command', () => {
+    describe('when command = generate-schema-migration' () => {
+      const command = 'npm run valkurm generate-schema-migration'
     })
   })
 })
